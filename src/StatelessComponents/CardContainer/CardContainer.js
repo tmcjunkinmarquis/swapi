8b383f7e2820223d6a8e6988cae1c33e7f1f3749
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-07-06 01:59:45 -0600

    Render people cards with name only

--- src/StatelessComponents/CardContainer/CardContainer.js
+++ src/StatelessComponents/CardContainer/CardContainer.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Card from '../Card/Card'
+import './CardContainer.css'
 
 const CardContainer = (props) =>{
-  
+  const { cards } = props
+
+  const makeCards = () => {
+    return cards.map(card =>{
+      return <Card name={card.name}/>
+    })
+  }
   
   return (
-    <div>
-      {()=>console.log(props)}
+    <div className="card-container">
+      {makeCards()}
     </div>
   )
 }
